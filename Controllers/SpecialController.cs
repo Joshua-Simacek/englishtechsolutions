1fee8a56ef8755587e110fcc1925814d63267ecd
Joshua <joshuasimacek@gmail.com>
2016-11-10 23:24:51 -0500

    initial

---
+++ Controllers/SpecialController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnglishTechSolutions.Controllers
+{
+    public class SpecialController : Controller
+    {
+        private readonly string pass = ConfigurationManager.AppSettings["special"];
+        // GET: Special
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Auth(string password)
+        {
+            if(password != pass)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Special", new { password });
+        }
+
+        public ActionResult Special(string password)
+        {
+            if(password != pass)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
